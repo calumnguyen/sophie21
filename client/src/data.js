4f563f27cf2c95390a4adf28dc41e2805680c1d1
Calum Nguyen <calumengee@calums-air.lan>
2021-01-03 20:05:28 -0800

    update

--- client/src/data.js
+++ client/src/data.js
@@ -24,46 +24,39 @@ function chillHop() {
         'https://res.cloudinary.com/calumnguyen/video/upload/v1609707430/NikAudio_xftylz.mp3',
       active: false,
     },
-    // {
-    //   id: uuidv4(),
-    //   name: 'Going Back',
-    //   artist: 'Sworn',
-    //   cover:
-    //     'https://chillhop.com/wp-content/uploads/2020/10/737bb830d34592344eb4a2a1d2c006cdbfc811d9-1024x1024.jpg',
-    //   color: ['#2B4F8B', '#2F4F5A'],
-    //   audio: 'https://mp3.chillhop.com/serve.php/?mp3=10448',
-    //   active: false,
-    // },
-    // {
-    //   id: uuidv4(),
-    //   name: 'Tales of a Flowing Forest',
-    //   artist: 'Leavv',
-    //   cover:
-    //     'https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg',
-    //   color: ['#329160', '#DF864D'],
-    //   audio: 'https://mp3.chillhop.com/serve.php/?mp3=9917',
-    //   active: false,
-    // },
-    // {
-    //   id: uuidv4(),
-    //   name: 'Reflection',
-    //   artist: 'Sworn',
-    //   cover:
-    //     'https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg',
-    //   color: ['#E27482', '#7D3376'],
-    //   audio: 'https://mp3.chillhop.com/serve.php/?mp3=10448',
-    //   active: false,
-    // },
-    // {
-    //   id: uuidv4(),
-    //   name: 'Tales of a Flowing Forest',
-    //   artist: 'Leavv',
-    //   cover:
-    //     'https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg',
-    //   color: ['#329160', '#DF864D'],
-    //   audio: 'https://mp3.chillhop.com/serve.php/?mp3=9917',
-    //   active: false,
-    // },
+    {
+      id: uuidv4(),
+      name: 'FROM: Grandpa & Granny',
+      artist: 'TO: Sophie',
+      cover:
+        'https://res.cloudinary.com/calumnguyen/image/upload/v1609714196/granmasoph_zeg2g6.png',
+      color: ['#f5b461', '#f3eac2'],
+      audio:
+        'https://res.cloudinary.com/calumnguyen/video/upload/v1609713792/GrandpaGrandmaAudio_b2eocv.mp3',
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: 'FROM: Claire, Steve and Alannah',
+      artist: 'TO: Sophie',
+      cover:
+        'https://res.cloudinary.com/calumnguyen/image/upload/v1609716366/clairealannahsoph_nnfxob.png',
+      color: ['#28abb9', '#2d6187'],
+      audio:
+        'https://res.cloudinary.com/calumnguyen/video/upload/v1609715970/ClaireAlaanahSteveAudio_m5d04a.mp3',
+      active: false,
+    },
+    {
+      id: uuidv4(),
+      name: 'FROM: Corrie',
+      artist: 'TO: Sophie',
+      cover:
+        'https://res.cloudinary.com/calumnguyen/image/upload/v1609717711/corriesoph_ud16pt.png',
+      color: ['#a6a9b6', '#eff8ffc'],
+      audio:
+        'https://res.cloudinary.com/calumnguyen/video/upload/v1609717597/CorrieAudio_t7qouu.mp3',
+      active: false,
+    },
   ];
 }
 
